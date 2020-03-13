b1381b8c173d19e8a8fbffc72227dd323fae4267
大西友香 <oonishiyuka@oonishiyukanoMacBook-ea.local>
2020-03-13 13:54:22 +0900

    ~sec-06

--- front-page.php
+++ front-page.php
@@ -81,35 +81,135 @@ get_header(); ?>
       <ul>
         <li class="flex">
           <div class="left">
-            <h3>業者用オンラインに<span>物件を登録</span></h3>
+            <h3>業者用オンラインに<span class="maker">物件を登録</span></h3>
             <p>即金買取させていただいた物件は、不動産流通機構（REINS：レインズ）へ登録いたします。<br>レインズは、全国で４つの公益社団法人や公益財団法人によって運営されております。<br>法律に基づいて設立・運営されている公益法人で、売却時にも公的な証明書（登録証明書）が発行されます。</p>
           </div>
           <div class="img-wrap">
-            <img class="pc-only" src="<?php echo $wp_url; ?>/lib/images/03_flow_01@2x.png" alt="流れイラスト">
+            <img src="<?php echo $wp_url; ?>/lib/images/03_flow_01@2x.png" alt="流れイラスト">
           </div>
         </li>
         <li class="flex">
           <div class="left">
-            <h3>全国の不動産会社が<span>物件を検索</span></h3>
+            <h3>全国の不動産会社が<span class="maker">物件を検索</span></h3>
             <p>レインズには全国の不動産業者が加入し、ネットワークで結ばれています。<br>そのため、より多くの不動産会社へ売却した物件の情報が配信される事になります。<br>また、物件を探している不動産業者からは、希望の物件情報を検索した結果見つかった物件になるため、成約までのスピード化が期待できます。</p>
           </div>
           <div class="img-wrap">
-            <img class="pc-only" src="<?php echo $wp_url; ?>/lib/images/03_flow_01@2x.png" alt="流れイラスト">
+            <img src="<?php echo $wp_url; ?>/lib/images/03_flow_02@2x.png" alt="流れイラスト">
           </div>
         </li>
         <li class="flex">
           <div class="left">
-            <h3><span>安心した取引</span></h3>
+            <h3><span class="maker">安心した取引</span></h3>
             <p>買主側の不動産業者もレインズに登録された正規の不動産業者のため、取引は比較的スムーズに進む事が期待できます。成約証明書も発行され、正当な取引が処理された証明にもなります。</p>
           </div>
           <div class="img-wrap">
-            <img class="pc-only" src="<?php echo $wp_url; ?>/lib/images/03_flow_01@2x.png" alt="流れイラスト">
+            <img src="<?php echo $wp_url; ?>/lib/images/03_flow_03@2x.png" alt="流れイラスト">
           </div>
         </li>
       </ul>
+      <div class="after">
+        <h3>
+          <img src="<?php echo $wp_url; ?>/lib/images/03_subttl.png" alt="その後の流れ">
+        </h3>
+        <div class="flex">
+          <div class="img-wrap">
+            <img src="<?php echo $wp_url; ?>/lib/images/03_after_01@2x.png" alt="売却レポート">
+          </div>
+          <div class="img-wrap sp-only">
+            <img src="<?php echo $wp_url; ?>/lib/images/03_arrow.png" alt="矢印">
+          </div>
+          <div class="img-wrap">
+            <img src="<?php echo $wp_url; ?>/lib/images/03_after_02@2x.png" alt="ボーナス入金">
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </section>
+
+<section id="sec-04">
+  <div class="sec">
+    <div class="wrap flex">
+      <div class="img-wrap">
+        <img src="<?php echo $wp_url; ?>/lib/images/04_img@2x.png" alt="ボーナス入金">
+      </div>
+      <div class="txt-wrap">
+        <h3>京都をベースに開発してきた<br>ガーネットだからこそ！</h3>
+        <p>オーナー、不動産、投資家など、顧客にとってベストな開発提案を考え続けてきた結果、顧客第一主義の徹底した透明性を重視したサービスこそが、最大限のメリットある利益拡大を実現する事が出来ました。<br>そして営業マン全員が宅建士ということから、レベルの高い提案ができるのです。</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="sec-05">
+  <div class="sec">
+    <div class="wrap">
+      <h2 class="ttl-wrap">
+        <img src="<?php echo $wp_url; ?>/lib/images/05_ttl.png" alt="SELL+の実績">
+      </h2>
+    </div>
+    <ul>
+      <?php
+      $arg = array(
+        'posts_per_page' => 3,
+        'orderby' => 'date',
+        'order' => 'DESC',
+        'post_type' => 'case',
+      );
+      $posts = get_posts($arg);
+      if ($posts): ?>
+      <?php foreach ($posts as $post): setup_postdata($post);
+      ?>
+      <li class="grad-wrap">
+        <span class="grad-trigger"></span>
+          <div class="grad-item flex">
+            <div class="txt-wrap">
+              <h3><?php the_field( 'ttl' ); ?></h3>
+              <h4>
+                売却額<span><?php the_field( 'price_01' ); ?></span>
+                + 利上げボーナス<span><?php the_field( 'price_02' ); ?></span>
+                =<span><?php the_field( 'price_03' ); ?>万円</span>
+              </h4>
+              <p><?php the_field( 'p' ); ?></p>
+              <table>
+                <tr>
+                  <th>物件の種類</th>
+                  <td><?php the_field( 'type' ); ?></td>
+                </tr>
+                <tr>
+                  <th>物件の種類</th>
+                  <td><?php the_field( 'size' ); ?></td>
+                </tr>
+              </table>
+            </div>
+            <div class="img-wrap">
+              <img src="<?php the_field( 'img' ); ?>" alt="<?php the_field( 'ttl' ); ?>">
+            </div>
+          </div>
+      </li>
+    <?php endforeach; ?>
+    <?php endif; wp_reset_postdata(); ?>
+    </ul>
+  </div>
+</section>
+
+<section id="sec-07">
+  <div class="sec">
+    <div class="wrap">
+      <h2 class="ttl-wrap">
+        <img src="<?php echo $wp_url; ?>/lib/images/07_ttl.png" alt="3STEPで簡単！速い！">
+      </h2>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
 
 <section id="sec-02">
   <div class="sec">
@@ -120,8 +220,6 @@ get_header(); ?>
     </div>
   </div>
 </section>
-
-
 
 
 <section id="sec-02">
